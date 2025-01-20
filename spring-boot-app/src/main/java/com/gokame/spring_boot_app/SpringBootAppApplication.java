0318bbe3dc74211f31ca3f2234a3c6c8e73886dc
MoKi <Mokiwins@gmail.com>
2025-01-20 12:28:41 -0500

    springBoot

---
+++ spring-boot-app/src/main/java/com/gokame/spring_boot_app/SpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.gokame.spring_boot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// start application here
+@SpringBootApplication
+public class SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppApplication.class, args);
+	}
+
+}
